d393e6a5cd11b4ea85ac8dec1b2204ea4e6d5c44
soumyamajumder2022-oss <soumyamajumder2022@gmail.com>
2025-10-20 22:09:46 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === 'production') {
     
     // Serve the React app for any route
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
     });
 }
 
